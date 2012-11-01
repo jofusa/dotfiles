6537e9984e9d54f2db1e437f5c5254696be9f6ae
jofusa <dennison.john@gmail.com>
2012-11-01 16:59:49 -0500

    new vundle command

--- symlink.sh
+++ symlink.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 if [ ! -d ~/.vim/UltiSnips/ ]; then
    echo "Making Directory"
    mkdir ~/.vim/UltiSnips	
 fi
 
-ls -1 ./UltiSnips/* | xargs -n1 -I{} sudo cp  {} ~/.vim/UltiSnips
+ls -1 ./UltiSnips/* | xargs -n1 -I{} cp  {} ~/.vim/UltiSnips
 
 
 cp ./vimrc ~/.vimrc
-
+ 
+vim +BundleInstall +qall
