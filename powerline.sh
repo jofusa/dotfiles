b1792e0ffc40bac514150d765606dd04ae4486c1
John Dennison <dennison.john@gmail.com>
2014-03-23 19:44:36 -0600

    powerline install

---
+++ powerline.sh
@@ -0,0 +1,7 @@
+
+
+pip install --user git+git://github.com/Lokaltog/powerline
+
+mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
